6cb70d08f477f669dfcade543bbc9848cdf4229e
Antoine Balliet <antoine.balliet@gorgias.com>
2024-12-18 16:50:38 +0100

    add test for config parsing unnest

---
+++ tests/destinations/test_config.py
@@ -0,0 +1,45 @@
+from bizon.destinations.config import AbstractDestinationConfig, AbstractDestinationDetailsConfig
+import pytest
+from pydantic import ValidationError
+
+def test_config():
+    config = AbstractDestinationConfig(
+        name="file",
+        config=AbstractDestinationDetailsConfig(
+            unnest=False,
+        )
+    )
+    assert config
+
+
+def test_config_no_record_schema_provided():
+
+    with pytest.raises(ValidationError) as e:
+        AbstractDestinationConfig(
+            name="file",
+            config=AbstractDestinationDetailsConfig(
+                unnest=True,
+            )
+        )
+
+
+def test_config_with_unnest_provided_schema():
+    config = AbstractDestinationConfig(
+        name="file",
+        config=AbstractDestinationDetailsConfig(
+            unnest=True,
+            record_schema=[
+                {
+                    "name": "name",
+                    "type": "string",
+                    "description": "Name of the user"
+                },
+                {
+                    "name": "age",
+                    "type": "int",
+                    "description": "Age of the user"
+                }
+            ]
+        )
+    )
+    assert config
