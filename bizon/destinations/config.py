f7781e84a8e62c78380f95bbf1dcec29058bbce1
Antoine Balliet <antoine.balliet@gorgias.com>
2024-12-18 16:50:16 +0100

    feat: assert schema is provided if unnest set to True

--- bizon/destinations/config.py
+++ bizon/destinations/config.py
@@ -2,7 +2,7 @@ from abc import ABC
 from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 
 class DestinationTypes(str, Enum):
@@ -33,19 +33,25 @@ class AbstractDestinationDetailsConfig(BaseModel):
         description="Maximum time in seconds for buffering after which the records will be written to the destination. Set to 0 to deactivate the timeout buffer check.",  # noqa
     )
 
-    unnest: Optional[bool] = Field(
-        default=False,
-        description="Unnest the data before writing to the destination. Schema should be provided in the model_config.",
-    )
-
     record_schema: Optional[list[DestinationColumn]] = Field(
         default=None, description="Schema for the records. Required if unnest is set to true."
+    )
+
+    unnest: bool = Field(
+        default=False,
+        description="Unnest the data before writing to the destination. Schema should be provided in the model_config.",
     )
 
     authentication: Optional[BaseModel] = Field(
         description="Authentication configuration for the destination, if needed", default=None
     )
 
+    @field_validator("unnest", mode="before")
+    def validate_record_schema_if_unnest(cls, value, values):
+        if bool(value) and values.data.get("record_schema") is None:
+            raise ValueError('A `record_schema` must be provided if `unnest` is set to True.')
+
+        return value
 
 class AbstractDestinationConfig(BaseModel):
     # Forbid extra keys in the model
