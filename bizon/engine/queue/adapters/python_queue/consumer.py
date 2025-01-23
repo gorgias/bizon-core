a47c4e0572b0b7df85a868689667c6c645faa1df
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2025-01-23 11:37:20 +0100

    fea: add datadog monitoring (#14)
    * fea: add datadog monitoring
    
    * chore: remove prometheus and otel
    
    * chore: handle no datadog
    
    * chore: lint

--- bizon/engine/queue/adapters/python_queue/consumer.py
+++ bizon/engine/queue/adapters/python_queue/consumer.py
@@ -10,6 +10,7 @@ from bizon.engine.pipeline.consumer import AbstractQueueConsumer
 from bizon.engine.pipeline.models import PipelineReturnStatus
 from bizon.engine.queue.config import QueueMessage
 from bizon.engine.queue.queue import AbstractQueue
+from bizon.monitoring.monitor import AbstractMonitor
 from bizon.transform.transform import Transform
 
 from .config import PythonQueueConfig
@@ -17,18 +18,29 @@ from .config import PythonQueueConfig
 
 class PythonQueueConsumer(AbstractQueueConsumer):
     def __init__(
-        self, config: PythonQueueConfig, queue: AbstractQueue, destination: AbstractDestination, transform: Transform
+        self,
+        config: PythonQueueConfig,
+        queue: AbstractQueue,
+        destination: AbstractDestination,
+        transform: Transform,
+        monitor: AbstractMonitor,
     ):
-        super().__init__(config, destination=destination, transform=transform)
+        super().__init__(
+            config,
+            destination=destination,
+            transform=transform,
+            monitor=monitor,
+        )
         self.queue = queue
+        self.monitor.track_pipeline_status(PipelineReturnStatus.RUNNING)
 
     def run(self, stop_event: Union[threading.Event, multiprocessing.synchronize.Event]) -> PipelineReturnStatus:
 
         while True:
-
             # Handle kill signal from the runner
             if stop_event.is_set():
                 logger.info("Stop event is set, closing consumer ...")
+                self.monitor.track_pipeline_status(PipelineReturnStatus.KILLED_BY_RUNNER)
                 return PipelineReturnStatus.KILLED_BY_RUNNER
 
             # Retrieve the message from the queue
---
+++ bizon/monitoring/datadog/monitor.py
@@ -0,0 +1,43 @@
+from datadog import initialize, statsd
+from loguru import logger
+
+from bizon.common.models import BizonConfig
+from bizon.engine.pipeline.models import PipelineReturnStatus
+from bizon.monitoring.monitor import AbstractMonitor
+
+
+class DatadogMonitor(AbstractMonitor):
+    def __init__(self, pipeline_config: BizonConfig):
+        super().__init__(pipeline_config)
+
+        # In Kubernetes, set the host dynamically
+        try:
+            initialize(
+                statsd_host=pipeline_config.monitoring.config.datadog_agent_host,
+                statsd_port=pipeline_config.monitoring.config.datadog_agent_port,
+            )
+        except Exception as e:
+            logger.info(f"Failed to initialize Datadog agent: {e}")
+
+        self.pipeline_monitor_status = "bizon_pipeline.status"
+        self.tags = [
+            f"name:{self.pipeline_config.name}",
+            f"stream:{self.pipeline_config.source.stream}",
+            f"source:{self.pipeline_config.source.name}",
+            f"destination:{self.pipeline_config.destination.name}",
+        ]
+
+        self.pipeline_active_pipelines = "bizon_pipeline.active_pipelines"
+
+    def track_pipeline_status(self, pipeline_status: PipelineReturnStatus) -> None:
+        """
+        Track the status of the pipeline.
+
+        Args:
+            status (str): The current status of the pipeline (e.g., 'running', 'failed', 'completed').
+        """
+
+        statsd.increment(
+            self.pipeline_monitor_status,
+            tags=self.tags + [f"status:{pipeline_status}"],
+        )
