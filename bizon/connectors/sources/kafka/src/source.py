3db1bd6c91442f01f8df5a1f6ca2ffdc20640563
Antoine Balliet <antoine.balliet@gorgias.com>
2025-04-18 13:53:27 +0200

    fix

--- bizon/connectors/sources/kafka/src/source.py
+++ bizon/connectors/sources/kafka/src/source.py
@@ -176,7 +176,7 @@ class KafkaSource(AbstractSource):
 
     def decode_avro(self, message: Message) -> dict:
         # Get the header bytes and the global id from the message
-        header_message_bytes = get_header_bytes(message.value())
+        header_message_bytes = get_header_bytes(nb_bytes_schema_id=self.config.nb_bytes_schema_id, message=message.value())
         global_id = parse_global_id_from_serialized_message(
             nb_bytes_schema_id=self.config.nb_bytes_schema_id,
             header_message_bytes=header_message_bytes,
