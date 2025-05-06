35126d66b7b34759a80b43791f56a73cfcd9ba2e
Antoine Balliet <antoine.balliet@gorgias.com>
2025-05-06 10:53:10 +0200

    chore: remove json debezium parsing

--- tests/connectors/sources/kafka/test_avro_decoding.py
+++ tests/connectors/sources/kafka/test_avro_decoding.py
@@ -11,9 +11,7 @@ from avro.schema import parse
 from bizon.connectors.sources.kafka.src.decode import (
     Hashabledict,
     decode_avro_message,
-    find_debezium_json_fields,
     get_header_bytes,
-    parse_debezium_json_fields,
     parse_global_id_from_serialized_message,
 )
 
@@ -107,47 +105,6 @@ def test_parse_global_id_unsupported():
     """Test parsing global ID with unsupported schema ID byte length"""
     with pytest.raises(ValueError):
         parse_global_id_from_serialized_message(6, b"123456")
-
-
-def test_find_debezium_json_fields(test_data):
-    """Test finding JSON fields in Debezium schema"""
-    json_fields = find_debezium_json_fields(test_data["hashable_schema"])
-
-    # Based on the provided schema, we expect "children" to be a JSON field
-    assert "children" in json_fields
-
-
-def test_parse_debezium_json_fields(test_data):
-    """Test parsing JSON fields from Debezium data"""
-    # Create a copy of data to test modification in-place
-    data_copy = test_data["data"].copy()
-
-    # Before parsing, the "children" field should be a string
-    assert isinstance(data_copy["after"]["children"], str)
-
-    # Parse the JSON fields
-    parse_debezium_json_fields(data_copy, test_data["hashable_schema"])
-
-    # After parsing, the "children" field should be parsed into a list/dict
-    assert isinstance(data_copy["after"]["children"], list)
-
-
-def test_parse_debezium_json_fields_with_emtpy_string(test_data):
-    """Test parsing JSON fields from Debezium data with a null value"""
-    # Create a copy of data to test modification in-place
-    data_copy = test_data["data"].copy()
-
-    # Set the "children" field to an empty string
-    data_copy["after"]["children"] = ""
-
-    # Before parsing, the "children" field should be a string
-    assert isinstance(data_copy["after"]["children"], str)
-
-    # Parse the JSON fields
-    parse_debezium_json_fields(data_copy, test_data["hashable_schema"])
-
-    # After parsing, the "children" field should be parsed as None
-    assert data_copy["after"]["children"] == None
 
 
 @patch("bizon.connectors.sources.kafka.src.decode.fastavro.schemaless_reader")
