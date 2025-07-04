7cddb8c11f40a9668f535d72bf5be6bb04fee5fa
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2025-07-04 12:03:59 +0200

    chore: make streaming_v2 work with clustering and orjson (#64)
    * chore: updated streaming v2 to make it work
    
    * chore: added retry strategy
    
    * chore: remove all use_legacy_streaming_api params
    
    * chore: remove all use_legacy_streaming_api params

--- bizon/connectors/sources/kafka/tests/kafka_pipeline.py
+++ bizon/connectors/sources/kafka/tests/kafka_pipeline.py
@@ -3,5 +3,5 @@ import os
 from bizon.engine.engine import RunnerFactory
 
 if __name__ == "__main__":
-    runner = RunnerFactory.create_from_yaml(filepath=os.path.abspath("test-pipeline-streaming.yml"))
+    runner = RunnerFactory.create_from_yaml(filepath=os.path.abspath("test-pipeline-streaming-v2.yml"))
     runner.run()
