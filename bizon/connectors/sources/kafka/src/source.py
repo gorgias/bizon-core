ab62d15ffa2cc6c8262549f60460615cbdd62b1d
Antoine Balliet <antoine.balliet@gorgias.com>
2025-07-07 12:20:05 +0200

    chore: stop skipping message too large

--- bizon/connectors/sources/kafka/src/source.py
+++ bizon/connectors/sources/kafka/src/source.py
@@ -237,13 +237,12 @@ class KafkaSource(AbstractSource):
             if message.error():
                 # If the message is too large, we skip it and update the offset
                 if message.error().code() == KafkaError.MSG_SIZE_TOO_LARGE:
-                    logger.warning(
+                    logger.error(
                         (
                             f"Message for topic {message.topic()} partition {message.partition()} and offset {message.offset()} has been skipped. "
                             f"Raised MSG_SIZE_TOO_LARGE, we suppose the message does not exist. Double-check in Confluent Cloud."
                         )
                     )
-                    continue
 
                 logger.error(
                     (
