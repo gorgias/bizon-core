41b62a64dd19820232b8ca6d4c4343dd8629c631
Antoine Balliet <antoine.balliet@gorgias.com>
2025-07-09 12:19:01 +0200

    fix

--- bizon/monitoring/noop/monitor.py
+++ bizon/monitoring/noop/monitor.py
@@ -1,11 +1,12 @@
-from bizon.common.models import BizonConfig
+from bizon.common.models import SyncMetadata
 from bizon.engine.pipeline.models import PipelineReturnStatus
+from bizon.monitoring.config import MonitoringConfig
 from bizon.monitoring.monitor import AbstractMonitor
 
 
 class NoOpMonitor(AbstractMonitor):
-    def __init__(self, pipeline_config: BizonConfig):
-        super().__init__(pipeline_config)
+    def __init__(self, sync_metadata: SyncMetadata, monitoring_config: MonitoringConfig):
+        super().__init__(sync_metadata, monitoring_config)
 
     def track_pipeline_status(self, pipeline_status: PipelineReturnStatus) -> None:
         pass
